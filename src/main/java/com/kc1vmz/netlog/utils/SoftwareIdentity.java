ac3d0f7ce06a9fba3d6375c219ace422857250b0
John Rokicki <jcrokicki@gmail.com>
2026-05-30 18:22:58 -0400

    Version 1.0.7

--- src/main/java/com/kc1vmz/netlog/utils/SoftwareIdentity.java
+++ src/main/java/com/kc1vmz/netlog/utils/SoftwareIdentity.java
@@ -22,5 +22,5 @@ package com.kc1vmz.netlog.utils;
 
 public class SoftwareIdentity {
     public final static String NAME = "NETLOG";
-    public final static String VERSION = "1.0.6";
+    public final static String VERSION = "1.0.7";
 }
